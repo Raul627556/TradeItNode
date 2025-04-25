fcd649374d8eb3d2a6605dce159ae443bce2dfd9
Raul <raulgimenezperez46@gmail.com>
2025-04-25 17:14:09 +0200

    asd

--- models/User.js
+++ models/User.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  country: String,
-  email: String,
-  age: Number,
-  last_login: Date,
-  photo_src: String,
-  location: String,
-  zipcode: String,
+  personalInfo: {
+    name: String,
+    email: String,
+    country: String,
+    age: Number,
+    photo_src: String,
+    location: String,
+    zipcode: String,
+  },
+  account: {
+    username: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+    loginAttempts: { type: Number, default: 0 },
+    lockUntil: { type: Date, default: null },
+    last_login: { type: Date, default: null },
+    tokens: [{ token: String }]
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
